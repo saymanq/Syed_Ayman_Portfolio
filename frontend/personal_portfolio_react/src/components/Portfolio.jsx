97e24c5907795aeaa0c24f64d5ad7ae1b7687fb9
saymanq <87442765+saymanq@users.noreply.github.com>
2024-07-01 18:28:30 +0300

    Changes Made

--- frontend/personal_portfolio_react/src/components/Portfolio.jsx
+++ frontend/personal_portfolio_react/src/components/Portfolio.jsx
@@ -19,6 +19,13 @@ const Portfolio = () => {
         fetchdata();
     }, [])
     // eval(data.image_url)
+
+    const images = {
+        project1: project1,
+        project2: project2,
+        project3: project3,
+        project4: project4,
+      };
     
     return (
     <section id="portfolio" className="flex justify-center items-center flex-col py-9">
@@ -28,11 +35,11 @@ const Portfolio = () => {
         <div className="flex w-full p-1 sm:mt-5">
             <div className="mx-5 sm:mt-5 mb-7 text-left overflow-auto w-full flex justify-evenly items-center flex-wrap cards-wrapper">
                 {dataList.map(data => {
-                    const img = eval(data.image_url)
+                    const img_mapping = data.image_url
                     return (
                         <a key={data.project_id} href={data.link_url} target="_blank">
                             <div className={`border-box border-2 border-two rounded-lg w-[300px] h-[350px] overflow-auto hover:shadow-two shadow-xl transition duration-0 hover:duration-300 ease-in-out ${((data.project_id) > (window.innerWidth >= 640 ? 3 : 1)) ? 'mt-14 mb-6' : ''} `}>
-                                <img src={img} className="rounded-lg w-full h-[160px] "/>
+                                <img src={images[img_mapping]} className="rounded-lg w-full h-[160px] "/>
                                 <div className="text-two mt-2 mx-2 text-[20px] text-center font-Montserrat tracking-tighter underline underline-offset-[6px]">{data.project_name}</div>
                                 <div className="text-white mt-2 mx-2 text-[17px] text-center font-Montserrat tracking-tighter">{data.project_description}</div>
                             </div>
